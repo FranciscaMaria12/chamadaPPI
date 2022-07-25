40aca336ac12209c8633011f10c6ff78b24b749a
FranciscaMaria12 <109979507+FranciscaMaria12@users.noreply.github.com>
2022-07-25 13:33:52 -0300

    Initial commit

---
+++ js/registra-chamada.js
@@ -0,0 +1,21 @@
+function presenca(){
+    let nome = document.querySelector("#txt_nome").value;
+    console.log("Nome: " + nome);
+
+    let tdNome = document.createElement("td");
+    let td = [];
+    for(let i = 0; i < 6; i++){
+        td[i]=document.createElement("td");
+        td[i].innerHTML="P";
+    }
+    tdNome.innerHTML = nome;
+
+    let tr = document.createElement("tr");
+    tr.appendChild(tdNome);
+
+    for(let i = 0; i < 6; i++){
+        tr.appendChild(td[i]);
+    }
+    let tbody = document.querySelector("#dados");
+    tbody.appendChild(tr);
+}
